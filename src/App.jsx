edad1884a57d148669019ac755349f190e34cdf4
Stas <stas@macmini.com>
2026-01-19 01:55:43 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+/* eslint-disable no-empty */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useState } from 'react'
+import { styled } from 'styled-components'
+import * as Web3 from 'react-web3-storage'
+
+const Body = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const InputPrivateText = ({ id, onChange, placeholder }) => {
+  const [state, setState, updateState, status] = Web3.usePrivateStorage(id, '')
+
+  useEffect(() => {
+    if (onChange) {
+      onChange(state)
+    }
+  }, [state, onChange])
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <input disabled={status.type === 'load' ||status.type === 'connect' || status.type === 'upload'} type="text" placeholder={placeholder} value={state} onChange={({ target: { value } }) => setState(value)} />
+      <button onClick={() => updateState()}>Save</button>
+      <div>{JSON.stringify(status)}</div>
+    </div>
+  )
+}
+
+const InputPublicText = ({ id, onChange, placeholder }) => {
+  const [state, setState, updateState, status] = Web3.useStorage(id, '')
+
+  useEffect(() => {
+    if (onChange) {
+      onChange(state)
+    }
+  }, [state, onChange])
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <input disabled={status.type === 'load' ||status.type === 'connect' || status.type === 'upload'} type="text" placeholder={placeholder} value={state} onChange={({ target: { value } }) => setState(value)} />
+      <button onClick={() => updateState()}>Save</button>
+      <div>{JSON.stringify(status)}</div>
+    </div>
+  )
+}
+
+const PublicText = ({ id, address }) => {
+  const [state, status] = Web3.useReadStorage(id, address)
+
+  return (
+    <div style={{ display: 'flex' }}>{state}, {JSON.stringify(status)}</div>
+  )
+}
+
+const InputTableText = ({ id }) => {
+  const [state, setState] = useState('')
+  const [items, addItem, updateItem, status] = Web3.useTableStorage(id)
+
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        <input placeholder={`table id: ${id}`} type="text" value={state} onChange={({ target: { value } }) => setState(value)} />
+        <button onClick={() => addItem(state)}>Add</button>
+      </div>
+      <div>{JSON.stringify(status)}</div>
+      <br />
+      <div>
+        {
+          status.type !== 'connect' 
+            ? (
+              items.filter(item => !item.isDelete).map(item => (
+                <div key={item.index}>
+                  {item.index} {item.text}
+                  <button onClick={() => updateItem(item.index, "new string!")}>edit</button>
+                  <button onClick={() => updateItem(item.index, "")}>delete</button>
+                </div>
+              ))
+            )
+            : null
+        }
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
+  const [isConnect, open] = Web3.useApp()
+
+  return (
+    <Body>
+      <button onClick={() => open()}>{isConnect ? 'wallet' : 'connect'}</button>
+      <InputPrivateText id='1' placeholder='private text' />
+      <InputPublicText id='1' placeholder='public text' />
+      <PublicText id='1' address='0x14dC79964da2C08b23698B3D3cc7Ca32193d9955' />
+      <InputTableText id='1' />
+      <InputTableText id='2' />
+    </Body>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as Web3 from 'react-web3-storage'
+import App from './App.jsx'
+import './index.css'
+
+const config = {
+  metadata: {
+    name: 'React Web3 Storage',
+    description: 'Example DApp with React Web3 Storage',
+    url: 'https://prohetamine.github.io/react-web3-storage/',
+    icons: ['https://prohetamine.github.io/react-web3-storage/icon.svg']
+  },
+  projectId: '1febfd92481d4ea997711d2ac4a363c0',
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Web3.Provider config={config}>
+       <App />
+    </Web3.Provider>
+  </StrictMode>
+)
